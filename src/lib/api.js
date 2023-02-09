016d98dffa2b544dfc2642f1fa105c7d10210a34
HyungMinKim <103104307+HyungMinKim98@users.noreply.github.com>
2023-02-09 11:13:33 +0900

    Update api.js
    update api

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 
-const API_KEY=process.env.REAC_APP_API_KEY;
+const API_KEY=process.env.REACT_APP_API_KEY;
 
 export const videoUrl=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&regionCode=kr&key=${API_KEY}`
 
